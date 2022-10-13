55b4be9e7a8bf368588bf7d9efbb71e89feb7c84
Abdullah Al Harun <aharun46@gmail.com>
2022-10-13 16:11:29 +0000

    creating detail and list view

--- blogdemo/blog/views.py
+++ blogdemo/blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
+def post_list_view(request):
+    posts = Posts.objects.all()
+    template_name = 'blog/post/list.html'
+    context = {'posts':posts}
+    return render(request, template_name, context)
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    template_name = 'blog/post/detail.html'
+    context = {'post':post}
+    return render(request, template_name, context)
