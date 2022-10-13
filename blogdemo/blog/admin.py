ea30d16025e497cf85e445e4ae51b3887eaf08c6
Abdullah Al Harun <aharun46@gmail.com>
2022-10-13 14:12:52 +0000

    post admin page more interective

--- blogdemo/blog/admin.py
+++ blogdemo/blog/admin.py
@@ -4,4 +4,9 @@ from .models import Post
 #admin.site.register(Post)
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_display = ['title', 'slug', 'author', 'publish', 'status'] # list display in admin
+    list_filter = ['status', 'created', 'publish', 'author'] # when we filter by this catetgory
+    search_fields = ['title', 'body'] # search fields
+    prepopulated_fields = {'slug': ('title',)} # when we create post and set title slug field aotomatic create by title
+    date_hierarchy =  'publish' # 
+    ordering = ['status', 'publish'] # 
