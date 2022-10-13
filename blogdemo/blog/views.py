b81cb260311ad3315f280f96334a48f45e62cc00
Abdullah Al Harun <aharun46@gmail.com>
2022-10-13 20:28:46 +0000

    post template add

--- blogdemo/blog/views.py
+++ blogdemo/blog/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
 def post_list_view(request):
-    posts = Posts.objects.all()
-    template_name = 'blog/post/list.html'
+    posts = Post.objects.all()
+    template_name = 'blog/post/post_list.html'
     context = {'posts':posts}
     return render(request, template_name, context)
 
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    template_name = 'blog/post/detail.html'
+    template_name = 'blog/post/post_detail.html'
     context = {'post':post}
     return render(request, template_name, context)
