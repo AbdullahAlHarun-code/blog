6debae82a7afcea543ae8de2f90a9dfa90997fa8
Abdullah Al Harun <aharun46@gmail.com>
2022-10-13 21:32:45 +0000

    convert post list function based to class based view and change url as well

--- blogdemo/blog/urls.py
+++ blogdemo/blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 app_name = 'blog'
 urlpatterns = [
-    path('', views.post_list_view, name='post_list_view'),
+    #path('', views.post_list_view, name='post_list_view'),
+    path('', views.PostListView.as_view(), name = 'post_list_view'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
--- blogdemo/blog/views.py
+++ blogdemo/blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/post_list.html'
+
 def post_list_view(request):
     posts = Post.objects.all()
     paginator = Paginator(posts, 3) # 3 post per page
