1bb2fd6f1d05d012d5c3f2a4f1ac213db9c0b633
Abdullah Al Harun <aharun46@gmail.com>
2022-10-18 11:22:29 +0000

    add share html file and add send_mail finctionality and share url path

--- blogdemo/blog/urls.py
+++ blogdemo/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name = 'post_list_view'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('facker/', views.fakerdemo, name='facker_demo'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
