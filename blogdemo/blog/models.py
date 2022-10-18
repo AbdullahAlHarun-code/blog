15eca78448840ff049b5f365cc49fe049bc4594b
Abdullah Al Harun <aharun46@gmail.com>
2022-10-18 11:47:09 +0000

    created comment model form

--- blogdemo/blog/models.py
+++ blogdemo/blog/models.py
@@ -40,11 +40,11 @@ class Comment(models.Model):
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
-    created = models.DateTimeField(auto_mow_add=True)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
     class Meta:
-        oredering = ['created']
+        ordering = ['created']
         indexes = [
             models.Index(fields=['created'])
         ]
